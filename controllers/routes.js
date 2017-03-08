131251300bf6f5d10fdf37ae2b960cf5669ffd61
dromestrome <ashutoshc8101@gmail.com>
2017-03-08 15:13:50 +0530

    Hey guys

--- controllers/routes.js
+++ controllers/routes.js
@@ -1,9 +1,12 @@
 module.exports = function(app,product,User,Cart){
 
 app.get("/",loggedIn,function(req,res){
+
   product.find({},function(err,result){
     if(err) res.send("error occured");
-    res.render("home",{"products": result});
+
+      res.render("home",{"products": result});
+
   });
 });
 
@@ -42,14 +45,45 @@ app.get("/logout",function(req,res){
 app.get("/addToCart",loggedIn,function(req,res){
   var product = new Cart({
     user_id : req.user._id,
-    product_id : req.params.id,
+    product_id : req.query.id,
   });
 
   product.save(function(){
     console.log("Product added to Cart");
-    res.redirect("/");
+    res.redirect("/cart");
   });
 
 });
+
+
+app.get("/cart",loggedIn,function(req,res){
+  var products = [];
+  var count = 0;
+  Cart.find({'user_id' : req.user._id},function(err,result){
+      if(result.length > 0){
+      for(i=0;i<result.length;i++){
+        product.findById(result[i].product_id, function(err,data){
+          products.push(data);
+          count++;
+          if(count === result.length){
+            console.log(products);
+            res.render("cart",{'products': products, "req":req});
+          }
+        });
+      }
+    }else{
+      res.render("cart",{"products": false, "req":req});
+    }
+  });
+});
+
+app.get("/removeProduct",loggedIn,function(req,res){
+
+  Cart.findOneAndRemove({"user_id": req.user._id, "product_id" : req.query.id },function(err,result){
+    res.redirect("/cart");
+  });
+
+});
+
 
 };
